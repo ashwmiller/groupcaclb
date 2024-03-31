23fbfc5f6cfc638d4a02e11be89ee893a0b0f561
daniisea <iseadani@gmail.com>
2024-03-31 17:02:19 -0400

    cursor code update

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ $(function(){
     //2-Image Pop-Up//
     $(".pop-image").hover(
         function() {
-            $(this).css("transform", "scale(1.2)");
+            $(this).css("transform", "scale(1.1)");
         },
         function() {
             $(this).css("transform", "scale(1)");
